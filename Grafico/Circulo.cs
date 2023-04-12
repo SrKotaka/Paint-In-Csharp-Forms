a747fbd7f6fe4c84e9bfbceff559b609313a379f
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-04-12 16:09:34 -0300

    Add files via upload

---
+++ Grafico/Circulo.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace Grafico
+{
+   internal class Circulo : Ponto
+   {
+      int raio;
+      public int Raio
+      {
+         get { return raio; }
+         set { raio = value; }
+      }
+      public Circulo(int xCentro, int yCentro, int novoRaio, Color novaCor) : // Construtor para a classe Circulo
+      base(xCentro, yCentro, novaCor)
+      {
+         raio = novoRaio;
+      }
+      public void SetRaio(int novoRaio)
+      {
+         raio = novoRaio;
+      }
+      public override void Desenhar(Color corDesenho, Graphics g)
+      {
+         Pen pen = new Pen(corDesenho);
+         g.DrawEllipse(pen, base.X - raio, base.Y - raio, // centro - raio
+         2 * raio, 2 * raio); // centro + raio
+      }
+      public override string ToString()
+      {
+         return TransformaString("c", 5) +
+                TransformaString(X, 5) +
+                TransformaString(Y, 5) +
+                TransformaString(Cor.R, 5) +
+                TransformaString(Cor.G, 5) +
+                TransformaString(Cor.B, 5) +
+                TransformaString(Raio, 5);
+      }
+   }
+}
---
+++ Grafico/Reta.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace Grafico
+{
+   class Reta : Ponto
+   {
+      private Ponto pontoFinal;
+
+      public Reta(int x1, int y1, int x2, int y2, Color novaCor) : // Construtor para a classe Reta
+      base(x1, y1, novaCor)
+      {
+         pontoFinal = new Ponto(x2, y2, novaCor);
+      }
+      public override void Desenhar(Color cor, Graphics g)
+      {
+         Pen pen = new Pen(cor);
+         g.DrawLine(pen, X, Y, pontoFinal.X, pontoFinal.Y);
+      }
+      public override string ToString()
+      {
+         return TransformaString("l", 5) +
+                TransformaString(X.ToString(), 5) +
+                TransformaString(Y.ToString(), 5) +
+                TransformaString(Cor.R, 5) +
+                TransformaString(Cor.G, 5) +
+                TransformaString(Cor.B, 5) +
+                TransformaString(pontoFinal.X, 5) +
+                TransformaString(pontoFinal.Y, 5);
+      }
+   }
+}
